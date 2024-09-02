7b60147a19cf29421c3b8a0c9684edf4dff7c9c5
Tim Ruscica <35348871+techwithtim@users.noreply.github.com>
2024-09-02 15:47:17 +0800

    Add files via upload

---
+++ api.py
@@ -0,0 +1,46 @@
+import enum
+from typing import Annotated
+from livekit.agents import llm
+import logging
+
+logger = logging.getLogger("temperature-control")
+logger.setLevel(logging.INFO)
+
+
+class Zone(enum.Enum):
+    LIVING_ROOM = "living_room"
+    BEDROOM = "bedroom"
+    KITCHEN = "kitchen"
+    BATHROOM = "bathroom"
+    OFFICE = "office"
+
+
+class AssistantFnc(llm.FunctionContext):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._temperature = {
+            Zone.LIVING_ROOM: 22,
+            Zone.BEDROOM: 20,
+            Zone.KITCHEN: 24,
+            Zone.BATHROOM: 23,
+            Zone.OFFICE: 21,
+        }
+
+    @llm.ai_callable(description="get the temperature in a specific room")
+    def get_temperature(
+        self, zone: Annotated[Zone, llm.TypeInfo(description="The specific zone")]
+    ):
+        logger.info("get temp - zone %s", zone)
+        temp = self._temperature[Zone(zone)]
+        return f"The temperature in the {zone} is {temp}C"
+
+    @llm.ai_callable(description="set the temperature in a specific room")
+    def set_temperature(
+        self,
+        zone: Annotated[Zone, llm.TypeInfo(description="The specific zone")],
+        temp: Annotated[int, llm.TypeInfo(description="The temperature to set")],
+    ):
+        logger.info("set temo - zone %s, temp: %s", zone, temp)
+        self._temperature[Zone(zone)] = temp
+        return f"The temperature in the {zone} is now {temp}C"
---
+++ main.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from dotenv import load_dotenv
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
+from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.plugins import openai, silero
+from api import AssistantFnc
+
+load_dotenv()
+
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+        ),
+    )
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    fnc_ctx = AssistantFnc()
+
+    assitant = VoiceAssistant(
+        vad=silero.VAD.load(),
+        stt=openai.STT(),
+        llm=openai.LLM(),
+        tts=openai.TTS(),
+        chat_ctx=initial_ctx,
+        fnc_ctx=fnc_ctx,
+    )
+    assitant.start(ctx.room)
+
+    await asyncio.sleep(1)
+    await assitant.say("Hey, how can I help you today!", allow_interruptions=True)
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
